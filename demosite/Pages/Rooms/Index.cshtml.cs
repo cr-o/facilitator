aee18030dac74e3996d51e92c25fbdcd88c5b698
c <demomanisaspy@gmail.com>
2018-11-26 17:58:29 -0500

    sort finished. all basic functions in place

--- demosite/Pages/Rooms/Index.cshtml.cs
+++ demosite/Pages/Rooms/Index.cshtml.cs
@@ -17,12 +17,65 @@ namespace demosite.Pages.Rooms
         {
             _context = context;
         }
+        public string RoomIDSort { get; set; }
+        public string RoomNameSort { get; set; }
+        public string FloorNameSort { get; set; }
+        public string BNameSort { get; set; }
+        public string BAddressSort { get; set; }
+       
 
         public IList<Room> Room { get;set; }
         public int RoomID { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
+            RoomIDSort = String.IsNullOrEmpty(sortOrder) ? "roomid_desc" : "";
+            RoomNameSort = sortOrder == "RoomName" ? "roomname_desc" : "RoomName";
+            FloorNameSort = sortOrder == "FloorName" ? "floorname_desc" : "FloorName";
+            BNameSort = sortOrder == "BName" ? "bname_desc" : "BName";
+            BAddressSort = sortOrder == "BAddress" ? "baddress_desc" : "BAddress";
+
+            IQueryable<Room> roomIQ = from r in _context.Room
+                                      select r;
+            switch (sortOrder)
+            {
+                case "roomid_desc":
+                    roomIQ = roomIQ.OrderByDescending(r => r.RoomID);
+                    break;
+                case "RoomName":
+                    roomIQ = roomIQ.OrderBy(r => r.RoomName);
+                    break;
+                case "roomname_desc":
+                    roomIQ = roomIQ.OrderByDescending(r => r.RoomName);
+                    break;
+                case "FloorName":
+                    roomIQ = roomIQ.OrderBy(r => r.Floor.FloorName);
+                    break;
+                case "floorname_desc":
+                    roomIQ = roomIQ.OrderByDescending(r => r.Floor.FloorName);
+                    break;
+                case "BName":
+                    roomIQ = roomIQ.OrderBy(r => r.Floor.Building.BName);
+                    break;
+                case "bname_desc":
+                    roomIQ = roomIQ.OrderByDescending(r => r.Floor.Building.BName);
+                    break;
+                case "BAddress":
+                    roomIQ = roomIQ.OrderBy(r => r.Floor.Building.BAddress);
+                    break;
+                case "baddress_desc":
+                    roomIQ = roomIQ.OrderByDescending(r => r.Floor.Building.BAddress);
+                    break;
+                default:
+                    roomIQ = roomIQ.OrderBy(r => r.RoomID);
+                    break;
+            }
+            Room = await roomIQ.AsNoTracking()
+                .Include(r => r.Floor)
+                .Include(r => r.Floor.Building)
+                .Include(r => r.Floor.Building.PersonsBuildings)
+                .ToListAsync();
+            /*
             Room = await _context.Room
                 .Include(r => r.Floor)
                 .Include(r => r.Floor.Building)
@@ -30,6 +83,7 @@ namespace demosite.Pages.Rooms
                 .AsNoTracking()
                 //.OrderBy(r => r.RoomName)
                 .ToListAsync();
+                */
         }
     }
 }
